d107d67616fb6d457bbee0b117fb6aa6008499a5
Egrimm_van_Horstmann <ryanhodges101@gmail.com>
2026-01-12 13:02:18 -0500

    Initial commit

---
+++ AGENTS.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: ISW2026
+- Unity version: Unity 6000.2.6f2
+- Active game object:
+  - Name: Player
+  - Tag: Untagged
+  - Layer: Default
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControls : MonoBehaviour
+{
+    [SerializeField] private int leftBoarder;
+    [SerializeField] private int rightBoarder;
+
+    [SerializeField] private Transform playerTransform;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector2 playerPos = playerTransform.position;
+
+        //Input.GetAxis("Horizontal");
+
+        playerPos.x = Input.GetAxis("Horizontal");
+
+        //ClampPosition();
+    }
+    void ClampPosition()
+    {
+        Vector2 pos = transform.position;
+
+        pos.x = Mathf.Clamp(pos.x, leftBoarder, rightBoarder);
+
+        transform.position = pos;
+    }
+}
